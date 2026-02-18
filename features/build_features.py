cb7c5d223c81342ed9d612540d9dc5fc7998d33e
daniloldn <112765545+daniloldn@users.noreply.github.com>
2026-02-18 19:12:45 +0000

    building features

--- features/build_features.py
+++ features/build_features.py
@@ -18,9 +18,12 @@ Path("data/features").mkdir(parents=True, exist_ok=True)
 
 #loading data and cleaning 
 for ticker in tickers:
-    df = pd.read_parquet(f"data/raw/{ticker}.parquet")
+    df = pd.read_parquet(f"data/clean/{ticker}.parquet")
+    df = df.sort_index()
     df["returns"] = df["Close"].pct_change()
     df["ma_20"] = df["Close"].rolling(window=20).mean()
     df["volatility"] = df["returns"].rolling(20).std()
     df["momentum"] = df["Close"] / df["ma_20"]
-    df.to_parquet(f"data/features/{ticker}.parquet")
+    df = df.dropna(subset=["returns", "ma_20", "volatility", "momentum"])
+    final_df = df[["Close", "returns", "ma_20", "volatility", "momentum"]]
+    final_df.to_parquet(f"data/features/{ticker}.parquet")
